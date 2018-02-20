c3bda9ae4131f1f7707b3e3ee54255c4fbfd451c
DezkaReid <elmaildeldezkareid@gmail.com>
2018-02-20 13:27:56 -0600

    App setup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App/App.js'
+
+const mainElement = document.getElementById('app')
+
+ReactDOM.render(<App title='Hello Academy' />, mainElement)
